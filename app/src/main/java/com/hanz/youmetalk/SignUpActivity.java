c470c680dfd927415faf4885f7581898ecd8c4be
Hannahhh_ZZ <61225353+Hannah-Zhang-H@users.noreply.github.com>
2024-09-22 22:35:58 +1200

    Add singup activity

--- app/src/main/java/com/hanz/youmetalk/SignUpActivity.java
+++ app/src/main/java/com/hanz/youmetalk/SignUpActivity.java
@@ -1,6 +1,7 @@
 package com.hanz.youmetalk;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,41 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.hanz.youmetalk.databinding.ActivityLoginBinding;
+import com.hanz.youmetalk.databinding.ActivitySignUpBinding;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding signUpLayout;
+    private CircleImageView imageViewCircle;
+    private TextInputEditText editTextEmailSignup, editTextPasswordSignup, editTextTextUserNameSignup;
+    private Button buttonRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Inflating the layout takes the XML source and makes View objects
+        signUpLayout = ActivitySignUpBinding.inflate(getLayoutInflater());
+        // Get the root View
+        setContentView(signUpLayout.getRoot());
+
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Assign values
+        imageViewCircle = signUpLayout.imageViewCircle;
+        editTextEmailSignup = signUpLayout.editTextEmailSignup;
+        editTextPasswordSignup = signUpLayout.editTextPasswordSignup;
+        editTextTextUserNameSignup = signUpLayout.editTextUserNameSignup;
+        buttonRegister = signUpLayout.buttonRegister;
+
     }
 }
