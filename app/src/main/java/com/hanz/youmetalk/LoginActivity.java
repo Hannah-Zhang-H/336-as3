038a9fab886dc25fd6feb7c73830035412f797e6
Hannahhh_ZZ <61225353+Hannah-Zhang-H@users.noreply.github.com>
2024-09-22 22:07:50 +1200

    Add LoginActivity

--- app/src/main/java/com/hanz/youmetalk/LoginActivity.java
+++ app/src/main/java/com/hanz/youmetalk/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.hanz.youmetalk;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.hanz.youmetalk.databinding.ActivityLoginBinding;
+import com.hanz.youmetalk.databinding.ActivityMainBinding;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding loginLayout;
+    private TextInputEditText editTextEmail, editTextPassword;
+    private Button buttonSignin;
+    private TextView textViewSignup, textViewForget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Inflating the layout takes the XML source and makes View objects
+        loginLayout = ActivityLoginBinding.inflate(getLayoutInflater());
+        // Get the root View
+        setContentView(loginLayout.getRoot());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Assign values
+        editTextEmail = loginLayout.editTextEmail;
+        editTextPassword = loginLayout.editTextPassword;
+        buttonSignin = loginLayout.buttonSignin;
+        textViewSignup = loginLayout.textViewSignup;
+        textViewForget = loginLayout.textViewForgetPassword;
     }
 }
