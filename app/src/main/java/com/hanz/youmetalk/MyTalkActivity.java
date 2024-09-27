4040f367d3151a7e50a1cc4a11b1ece8ea0379d2
Hannahhh_ZZ <61225353+Hannah-Zhang-H@users.noreply.github.com>
2024-09-27 12:22:06 +1200

    Add the talk activity design

--- app/src/main/java/com/hanz/youmetalk/MyTalkActivity.java
+++ app/src/main/java/com/hanz/youmetalk/MyTalkActivity.java
@@ -1,24 +1,47 @@
 package com.hanz.youmetalk;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.hanz.youmetalk.databinding.ActivityMyTalkBinding;
 
 public class MyTalkActivity extends AppCompatActivity {
+
+    private ActivityMyTalkBinding myTalkLayout;
+    private RecyclerView recyclerViewMessageArea;
+    private ImageView imageViewBack;
+    private TextView textViewChatFriendName;
+    private EditText editTextMessage;
+    private FloatingActionButton fab;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        myTalkLayout = ActivityMyTalkBinding.inflate(getLayoutInflater());
+        setContentView(myTalkLayout.getRoot());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_my_talk);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerViewMessageArea = myTalkLayout.recyclerViewTalkArea;
+        imageViewBack = myTalkLayout.imageViewBack;
+        textViewChatFriendName = myTalkLayout.textViewChatFriendName;
+        editTextMessage = myTalkLayout.editTextMessage;
+        fab = myTalkLayout.fab;
+
     }
 }
