7b1c948f78c77661d6beaa0350b2a0d37d7b874d
Hannahhh_ZZ <61225353+Hannah-Zhang-H@users.noreply.github.com>
2024-10-25 17:03:40 +1300

    Upgrade UI

--- app/src/main/java/com/hanz/youmetalk/MainActivity.java
+++ app/src/main/java/com/hanz/youmetalk/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
